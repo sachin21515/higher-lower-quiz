2cf3e9a4a0c2c9ad0b224dc96d43e0b3bb38717e
sachin21515 <82361979+sachin21515@users.noreply.github.com>
2022-10-11 01:58:03 -0400

    Create Python.py
    uploading my code from the laptop to github

---
+++ Python.py
@@ -0,0 +1,48 @@
+
+from game_data import data
+import random
+import os
+
+current_score = [0]
+
+def check_followers():
+  num1 = random.randint(0, len(data)-1)
+  
+  num2 = [0]
+  def num():
+    num2[0]=(random.randint(0, len(data)-1))
+    if num1==num2[0]:
+      num()
+  num()  
+  
+  
+  
+  option_a = f"Compare A: {data[num1]['name']}, a {data[num1]['description']}, from {data[num1]['country']} "
+  print(option_a)
+  
+  option_b = f"Compare B: {data[num2[0]]['name']}, a {data[num2[0]]['description']}, from {data[num2[0]]['country']} "
+  print(option_b)
+  
+  user_input = input("Who has more followers? Type 'A' or 'B': ")
+  
+  if user_input.lower() == 'a':
+    if data[num1]['follower_count']>data[num2[0]]['follower_count']:
+      current_score[0]+=1
+      os.system('clear')
+      print(f"You are right! Current score: {current_score[0]}")
+      check_followers()
+    else:
+      os.system('clear')
+      print(f"sorry that's wrong Final score: {current_score[0]}")
+  
+  else:
+    if data[num1]['follower_count']<data[num2[0]]['follower_count']:
+      current_score[0]+=1
+      os.system('clear')
+      print(f"You are right! Current score: {current_score[0]}")
+      check_followers()
+    else:
+      os.system('clear')
+      print(f"sorry that's wrong Final score: {current_score[0]}")
+
+check_followers()
